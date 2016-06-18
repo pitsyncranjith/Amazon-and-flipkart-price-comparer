39d3fd276b4b68bdbc361368b4d162bcf759702c
codyowl <codyowl@gmail.com>
2016-06-18 23:58:49 +0530

    splitting the keyword

--- price_comparer.py
+++ price_comparer.py
@@ -1,9 +1,15 @@
 import urllib2
-from bsr import BeautifulSoup
+from bs4 import BeautifulSoup
 
 AMAZON_URL = "http://www.amazon.in/"
 
 FLIPKART_URL = "http://www.flipkart.com/"
+
+SEARCH_KEYWORD = raw_input("Enter the product name to compare :")
+
+SEARCH_KEYWORD = SEARCH_KEYWORD.lower()
+
+SPLITED_KEYWORD = SEARCH_KEYWORD.split()
 
 def seracher(url):
 	pass
